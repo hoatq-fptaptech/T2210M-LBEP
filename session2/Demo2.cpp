f8017ab432a2acd4b6e85b570590c81413b414f0
quanghoa <quanghoa@gmail.com>
2022-10-14 04:52:06 +0700

    session 2

---
+++ session2/Demo2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int x = 10;
+	float y = 3.14;
+	double z = -15.00213;
+	char h = 'A';
+	char ch = '1';
+	
+	printf("x = %d \n",x);
+	printf("y = %f \n",y);
+	printf("z = %lf \n",z);
+	printf("h = %c \n",h);
+}
