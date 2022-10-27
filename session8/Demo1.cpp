24c6dad09d7db45fa034c475be7721cefd437e05
quanghoa <hoatq4@fpt.edu.vn>
2022-10-28 04:39:33 +0700

    session 8

--- session8/Demo1.cpp
+++ session8/Demo1.cpp
@@ -8,6 +8,15 @@ int timMax(int arr[],int n){
 	}
 	return h;
 }
+int timMax2(int *arr,int n){
+	int h = arr[0];
+	for(int i=0;i<n;i++){
+		if(arr[i]>h){
+			h = arr[i];
+		}
+	}
+	return h;
+}
 int main(){
 	int n;
 	scanf("%d",&n);
